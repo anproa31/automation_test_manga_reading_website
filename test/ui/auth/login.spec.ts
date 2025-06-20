f66bc0fc99408b83fcbfc269c692c1e8b5c0e4c1
ANBUIII\antoe <antoeta31@gmail.com>
2025-06-21 05:56:23 +0700

    refactor: move data to new file

--- test/ui/auth/login.spec.ts
+++ test/ui/auth/login.spec.ts
@@ -1,4 +1,12 @@
 import LoginPage from '../pages/LoginPage';
+import {
+    VALID_USERNAME,
+    VALID_PASSWORD,
+    USERNAME_WITH_SPACES,
+    PASSWORD_WITH_SPACES,
+    UPPERCASE_USERNAME,
+    USERNAME_REQUIRED_ERROR
+} from '../data/testData';
 
 const loginPage = new LoginPage();
 
@@ -6,43 +14,29 @@ fixture`Login - Mangakatana`
     .page`https://mangakatana.com/`;
 
 test('Login with valid credentials', async (t) => {
-    const username = 'auto_test';
-    
-    await loginPage.login(username, 'test@123');
+    await loginPage.login(VALID_USERNAME, VALID_PASSWORD);
 
-    await t.expect(loginPage.displayName.withText(username).exists).ok();
+    await t.expect(loginPage.displayName.withText(VALID_USERNAME).exists).ok();
 });
 
 test('Login with credentials containing leading/trailing spaces', async (t) => {
-    const username = 'auto_test';
-    const usernameWithSpaces = '   auto_test    ';
-    const passwordWithSpaces = ' test@123    ';
+    await loginPage.login(USERNAME_WITH_SPACES, PASSWORD_WITH_SPACES);
 
-    await loginPage.login(usernameWithSpaces, passwordWithSpaces);
-
-    await t.expect(loginPage.displayName.withText(username).exists).ok();
+    await t.expect(loginPage.displayName.withText(VALID_USERNAME).exists).ok();
 });
 
 test('Login with uppercase username and valid password', async (t) => {
-    const expectedUsername = 'auto_test';
-    const uppercaseUsername = 'Auto_test';
-    const password = 'test@123';
+    await loginPage.login(UPPERCASE_USERNAME, VALID_PASSWORD);
 
-    await loginPage.login(uppercaseUsername, password);
-
-    await t.expect(loginPage.displayName.withText(expectedUsername).exists).ok();
+    await t.expect(loginPage.displayName.withText(VALID_USERNAME).exists).ok();
 });
 
 test('Login with empty username', async (t) => {
-    const password = 'test@123';
-    const expectedError = 'Username is required!';
-
-
     await t
         .click(loginPage.loginLink)
-        .typeText(loginPage.passwordInput, password)
+        .typeText(loginPage.passwordInput, VALID_PASSWORD)
         .click(loginPage.submitButton);
 
-    await t.expect(loginPage.usernameError.withText(expectedError).exists).ok();
+    await t.expect(loginPage.usernameError.withText(USERNAME_REQUIRED_ERROR).exists).ok();
 });
 
---
+++ test/ui/data/testData.ts
@@ -0,0 +1,6 @@
+export const VALID_USERNAME = 'auto_test';
+export const VALID_PASSWORD = 'test@123';
+export const USERNAME_WITH_SPACES = '   auto_test    ';
+export const PASSWORD_WITH_SPACES = ' test@123    ';
+export const UPPERCASE_USERNAME = 'Auto_test';
+export const USERNAME_REQUIRED_ERROR = 'Username is required!'; 
