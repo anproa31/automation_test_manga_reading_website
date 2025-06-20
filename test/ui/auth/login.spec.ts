e7d8c02dd03c515be9ae9c92b27e7cd761101a2b
ANBUIII\antoe <antoeta31@gmail.com>
2025-06-21 05:47:16 +0700

    feat: add login with leading/trailing space

--- test/ui/auth/login.spec.ts
+++ test/ui/auth/login.spec.ts
@@ -13,3 +13,13 @@ test('Login with valid credentials', async (t) => {
     await t.expect(loginPage.displayName.withText(username).exists).ok();
 });
 
+test('Login with credentials containing leading/trailing spaces', async (t) => {
+    const username = 'auto_test';
+    const usernameWithSpaces = '   auto_test    ';
+    const passwordWithSpaces = ' test@123    ';
+
+    await loginPage.login(usernameWithSpaces, passwordWithSpaces);
+
+    await t.expect(loginPage.displayName.withText(username).exists).ok();
+});
+
