b774b445a02164c6a3eacf919a1b48e8fcf72ad4
ANBUIII\antoe <antoeta31@gmail.com>
2025-06-23 11:19:47 +0700

    refactor: change valid account and remove unnesscary data

--- test/ui/auth/data/loginTestData.ts
+++ test/ui/auth/data/loginTestData.ts
@@ -1,11 +1,9 @@
 export const VALID_USERNAME = 'auto_test';
-export const VALID_PASSWORD = 'test@ 123';
+export const VALID_PASSWORD = 'test@123';
 export const USERNAME_WITH_SPACES = '   auto_test    ';
-export const PASSWORD_WITH_SPACES = ' test@ 123    ';
+export const PASSWORD_WITH_SPACES = ' test@123    ';
 export const UPPERCASE_USERNAME = 'Auto_test';
 export const INVALID_USERNAME = 'invalid_username';
 export const INVALID_PASSWORD = 'invalid_password';
 export const VALID_EMAIL = 'antoeta31@gmail.com';
 export const SQL_INJECTION_STRING = "' OR '1'='1' --";
-export const MAX_LENGTH_USERNAME = 'auto_test_1';
-export const LONG_PASSWORD_ATTEMPT = 'A1b2C3d4E5f6G7h8I9j0K1l2M3n4O5p6Q7r8S9t0U1v2W3x4Y5z6A7b8C9d0E1f2G3h4I5j6K7l8M9n0O1p2Q3r4S5t6U7v8W9x0Y1z2A3b4C5d6E7f8G9h0I1j2K3l4M5n6O7p8Q9r0S1t2U3v4W5x6Y7z8A9b0C1d2E3f4G5h6I7j8K9l0M1n2O3p4Q5r6S7t8U9v0W1x2Y3z4A5b6C7d8E9f0G1h2I3j4K5l6M7n8O9p0Q1r2S3t4U5v6W7x8Y9z0.';
