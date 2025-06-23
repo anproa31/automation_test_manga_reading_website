469be2024d350b43fad0ff83f6e06f10589c81e6
ANBUIII\antoe <antoeta31@gmail.com>
2025-06-23 13:58:23 +0700

    chore: ignore data folder

--- test/ui/auth/data/loginTestData.ts
+++
@@ -1,9 +0,0 @@
-export const VALID_USERNAME = 'auto_test';
-export const VALID_PASSWORD = 'test@123';
-export const USERNAME_WITH_SPACES = '   auto_test    ';
-export const PASSWORD_WITH_SPACES = ' test@123    ';
-export const UPPERCASE_USERNAME = 'Auto_test';
-export const INVALID_USERNAME = 'invalid_username';
-export const INVALID_PASSWORD = 'invalid_password';
-export const VALID_EMAIL = 'antoeta31@gmail.com';
-export const SQL_INJECTION_STRING = "' OR '1'='1' --";
